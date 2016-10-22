a76ff5ff05b41e9ca6c9cf0887441d61883ad043
asteph06 <ashtoncaulfieldstephens@gmail.com>
2016-10-22 14:33:15 -0400

    javascript functions for making debugging easier

---
+++ mbta/mbtaTestFuncs.js
@@ -0,0 +1,33 @@
+<!--
+dent = "";
+
+function debug(funcPlace,funcName)
+{
+	var tab  = "    ";
+	var tabs = dent.length/tab.length;
+	if(funcPlace == "start"){
+		tabs = tabs + 1;
+		funcPlace = funcPlace+"ing "
+	}else if(funcPlace == "end"){
+		tabs = tabs - 1;
+		funcPlace = tab+funcPlace+"ing   ";
+	}else{
+		console.log("! -- initDent called incorrectly");
+		return;
+	}
+	if(tabs < 0){
+		console.log("! -- updateDent not called at start of a func");
+		return;
+	}
+	dent = "";
+	for(var i = 0; i<tabs; i++){
+		dent += tab;
+	}
+	console.log( dent + funcPlace + "function: " + funcName);
+	return;
+}
+
+function conLog(conMessage){
+	console.log( dent + conMessage);
+}
+-->
